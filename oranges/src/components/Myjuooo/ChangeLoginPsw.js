59d6b87b2eaed2db83839f63a44895f032368def
Cecilia L <1093860879@qq.com>
2019-09-18 23:42:06 +0800

    没写出什么实质性的东西

--- oranges/src/components/Myjuooo/ChangeLoginPsw.js
+++ oranges/src/components/Myjuooo/ChangeLoginPsw.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {NavLink,withRouter} from "react-router-dom";
-
+import MyjuoooTop from './MyjuoooTop'
+import { Icon } from 'antd';
+import '../../assets/myjuooo/css/ChangeLoginPsw.css'
 class ChangeLoginPsw extends React.Component{
     render(){
         return(
             <div>
-                <p onClick={()=>{this.props.history.goBack()}}>{"<"}</p>
-                我是修改密码
+                <MyjuoooTop msg={"修改登录密码"} dis={"none"}></MyjuoooTop>
+                <p className={"changLoginPSW_tit"}>为保障您的账户安全，修改前请先验证原密码</p>
+                <p className={"changLoginPSW_inp"}>
+                    <Icon type="shopping" className={"changLoginPSW_ico"}/>
+                    <input  type={"text"} placeholder={"输入当前密码"}/>
+                </p>
+                <div className={"changLoginPSW_btn"}>下一步</div>
             </div>
         )
     }
--- oranges/src/components/Myjuooo/Mysecurity.js
+++ oranges/src/components/Myjuooo/Mysecurity.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {NavLink,withRouter} from "react-router-dom";
+import MyjuoooTop from './MyjuoooTop'
+import { Icon } from 'antd';
+import '../../assets/myjuooo/css/Mysecurity.css'
+import mypic from '../../assets/myjuooo/img/myjuooo-mypic.png'
 // 如果使用createBrowserHistory
 // // import {createBrowserHistory} from 'history'
 // import creatHistory from 'history/createBrowserHistory'
@@ -10,14 +14,51 @@ class Mysecurity extends React.Component{
 
         return(
             <div>
-                <p onClick={()=>{this.props.history.goBack()}}>{"<"}</p>
-                <p>我是设置</p>
+                <MyjuoooTop msg={"账户设置"} dis={"block"}></MyjuoooTop>
+                {/*设置*/}
 
-                <p><NavLink activeStyle={{color:"red"}} exact to={"/Myjuooo/changeLoginPsw"}  >修改密码</NavLink></p>
-                <p><NavLink activeStyle={{color:"red"}} exact to={"/Myjuooo/payPsw/type/set"}  >支付密码</NavLink></p>
-                <p><NavLink activeStyle={{color:"red"}} exact to={"/Myjuooo/myRealNameCertification"}  >实名认证</NavLink></p>
-                <p><NavLink activeStyle={{color:"red"}} exact to={"/passport/login"}  >退出登录</NavLink></p>
+                <div className={"Mysecurity_mymsg"}>
+                    {/*我的信息*/}
+                    <div className={"Mysecurity_mypic"}>
+                        <span>头像</span>
+                        <img src={mypic}/>
+                    </div>
+                    <div className={"Mysecurity_myname"}>
+                        <span>昵称</span>
+                    </div>
+                </div>
 
+                <div className={"Mysecurity_myset"}>
+                    {/*支付信息*/}
+                    <div className={"Mysecurity_myname Mysecurity_mine"}>
+                        <span>手机号</span>
+                        <span style={{color:"#999999",fontSize:"12px"}}>186****8289</span>
+                    </div>
+                    <div className={"Mysecurity_myname Mysecurity_mine"}>
+                        <span>邮箱</span>
+                        <span></span>
+                    </div>
+                    <div className={"Mysecurity_myname Mysecurity_mine"}>
+                        <span>生日信息&nbsp;&nbsp;<i style={{color:"#cccccc",fontSize:"11px"}}>(只允许设置一次)</i></span>
+                        <span style={{color:"#999999",fontSize:"0.256rem"}}>去完善<Icon type="right" style={{color:"#d8d8d8"}} /></span>
+                    </div>
+                    <div className={"Mysecurity_myname Mysecurity_mine"}>
+                        <span>登录密码&nbsp;&nbsp;</span>
+                        <NavLink to={"/Myjuooo/changeLoginPsw"} style={{color:"#999999",fontSize:"0.256rem"}} >修改<Icon type="right" style={{color:"#d8d8d8"}} /></NavLink>
+                    </div>
+                    <div className={"Mysecurity_myname Mysecurity_mine"}>
+                        <span>支付密码&nbsp;&nbsp;</span>
+                        <NavLink to={"/Myjuooo/payPsw/type/set"} style={{color:"#999999",fontSize:"0.256rem"}} >设置<Icon type="right"  style={{color:"#d8d8d8"}}/></NavLink>
+                    </div>
+                    <div className={"Mysecurity_myname Mysecurity_mine"}>
+                        <span>实名认证&nbsp;&nbsp;</span>
+                        <NavLink to={"/Myjuooo/myRealNameCertification"} style={{color:"#999999",fontSize:"0.256rem"}} >未认证<Icon type="right" style={{color:"#d8d8d8"}} /></NavLink>
+                    </div>
+                </div>
+                {/*按钮*/}
+                <div className={"Mysecuiity_btn"}>
+                    <NavLink to={"/passport/login"}  >退出登录</NavLink>
+                </div>
 
             </div>
         )
